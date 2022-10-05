ba8403ec00c815903442da6708d1a49869ca0cb8
Mohit Acharya <35497915+hexmo@users.noreply.github.com>
2022-10-05 17:07:18 +0545

    feat: specs for home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root to: 'home#index' # Defines the root path route ("/")
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  root 'home#index'
 end
